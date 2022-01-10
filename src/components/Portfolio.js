608d3e6665bccb211e787e90ac4a798113686bf2
Blink <tipsyblink11@gmail.com>
2022-01-09 16:39:10 -0800

    update

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,5 @@
-import todonotes from '../images/todonotes.png'
+import todonotes from '../images/todo-notes.png'
+import todoAppReactjs from '../images/todo-app-reactjs.png'
 import './Portfolio.css'
 
 const Portfolio = () => {
@@ -14,12 +15,21 @@ const Portfolio = () => {
                 <a className="projDiv" href="https://todonotes.surge.sh/" target="_blank" rel="noreferrer">Live View</a>
             </div>
 
-            <div className="projBox"> 
+            <div className="projBox">
+                <img src={todoAppReactjs} alt="" />
+                <p>Todo App</p>
+                <div>
+                    <span>React Js</span> <span>CSS</span>
+                </div>
+                <a className="projDiv" href="https://kaisendev.github.io/todoapp/" target="_blank" rel="noreferrer">Live View</a>
+            </div>
+
+            {/*<div className="projBox"> 
                 <section className="upcoming">
-                    <p>Upcoming React Project</p>
+                    <p>Upcoming Project</p>
                     <span>Lorem Ipsum</span><span>Lorem Ipsum</span>
                 </section>
-            </div>
+            </div> */}
         </div>   
      );
 }

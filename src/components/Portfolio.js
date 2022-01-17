950cb0659afe3d2e85a6e88f20525496d9508710
Blink <tipsyblink11@gmail.com>
2022-01-16 17:18:35 -0800

    update

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,34 +1,31 @@
-import todonotes from '../images/todo-notes.png'
-import todoAppReactjs from '../images/todo-app-reactjs.png'
-import todoFirebase from '../images/todo-firebase.png'
+
 import './Portfolio.css'
 
 const Portfolio = () => {
     return ( 
         
         <div className="projContainer">  
-
-          <div className="projBox">
-                <img src={todoFirebase} alt="" />
-                <p>Todo App with Firebase</p>
+            <div className="projBox">
+                <div className='todo-fb-img'></div>
+                <p>Todo App</p>
                 <div>
-                    <span>React Js</span> <span>Firebase</span>
+                    <span>React Js</span> <span>Firebase</span> <span>CSS</span>
                 </div>
                 <a className="projDiv" href="https://kaisendev.github.io/todo-firebase/" target="_blank" rel="noreferrer">Live</a>
-            </div>      
-          
+            </div>     
+
             <div className="projBox">
-                <img src={todoAppReactjs} alt="" />
+                <div className='todo-react-img'></div>
                 <p>Todo App</p>
                 <div>
                     <span>React Js</span> <span>CSS</span>
                 </div>
                 <a className="projDiv" href="https://kaisendev.github.io/todoapp/" target="_blank" rel="noreferrer">Live</a>
-            </div>
-
+            </div> 
+          
             <div className="projBox">
-                <img src={todonotes} alt="" />
-                <p>Todo Notes</p>
+                <div className='todo-js-img'></div>
+                <p>Todo App</p>
                 <div>
                     <span>Javascript</span> <span>Bootstrap</span>
                 </div>
